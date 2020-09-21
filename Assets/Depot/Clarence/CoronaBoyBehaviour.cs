f70cb67f503d3fe8288d4323ddd12ec0c32a0e7c
Clarence <diyobe2207@gmail.com>
2020-09-21 17:01:55 +0200

    Test scripts for corona boy

---
+++ Assets/Depot/Clarence/CoronaBoyBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoronaBoyBehaviour : MonoBehaviour
+{
+    public List<Transform> objectives;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Objective")
+        {
+            collision.gameObject.GetComponent<Objective>().isMasked = false;
+        }
+    }
+}
