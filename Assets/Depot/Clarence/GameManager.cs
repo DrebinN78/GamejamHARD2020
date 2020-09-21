1b0a202ace586b93eb38a777dec3ede0bc0309b4
Clarence <diyobe2207@gmail.com>
2020-09-21 18:29:23 +0200

    - Implemented DOTween - Implemented GameManager

---
+++ Assets/Depot/Clarence/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public float timer = 60f;
+    public List<CoronaBoyBehaviour> unMaskedPeople;
+    public List<CoronaBoyBehaviour> maskedPeople;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    
+}
