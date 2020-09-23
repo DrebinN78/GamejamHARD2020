f83743e337cad820a4283bd50982f142bec5bf54
Clarence <diyobe2207@gmail.com>
2020-09-23 17:40:32 +0200

    Added Cop arms animations

---
+++ Assets/Depot/Clarence/CorocopAnimationsEvent.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CorocopAnimationsEvent : MonoBehaviour
+{
+    [SerializeField] private GameObject sparkTaser;
+    [SerializeField] private Transform sparkInitialPosition;
+
+    public void DisableSelf()
+    {
+        if (gameObject != null && gameObject.activeSelf)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
