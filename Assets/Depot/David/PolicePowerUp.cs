2e33439840b980f733865b1335b6b146937767a6
David <synlike@gmail.com>
2020-09-23 15:45:58 +0200

    Corocop Prefab updated with Ability 2 (wip)

--- Assets/Depot/David/PolicePowerUp.cs
+++ Assets/Depot/David/PolicePowerUp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 using Rewired;
 
 public class PolicePowerUp : MonoBehaviour
@@ -15,23 +16,33 @@ public class PolicePowerUp : MonoBehaviour
     
     };
 
-    private float timer = 0;
-
     [System.Serializable]
     struct PowerUp2
     {
+        public float immobilizationDuration;
         public float timeToRecharge;
+        public float maxDistance;
+        public bool taserOut;
+        public bool hasShot;
+        public LayerMask layermask;
+
     };
+
+    private float timer1 = 0;
+    private float timer2 = 0;
 
     [SerializeField]
     private PowerUp1 powerUp1;
-    // [SerializeField]
+    [SerializeField]
     private PowerUp2 powerUp2;
     
     private Player coroCop;
+    private Camera cam;
 
     private void Start()
     {
+        cam = GetComponentInChildren<Camera>();
+
         if(GameManager.instance.p1Choice == 1)
         {
             coroCop = ReInput.players.GetPlayer(1);
@@ -60,23 +71,51 @@ public class PolicePowerUp : MonoBehaviour
 
         if (powerUp1.abilityRecharging)
         {
-            timer += Time.deltaTime;
+            timer1 += Time.deltaTime;
 
-            if (timer > powerUp1.outlineDuration && powerUp1.isOutlined)
+            if (timer1 > powerUp1.outlineDuration && powerUp1.isOutlined)
             {
                 GameObject.Find("Player_Coronaboy").GetComponent<Outline>().OutlineWidth = 0;
                 powerUp1.isOutlined = false;
             }
-            if (timer > powerUp1.timeToRecharge)
+            if (timer1 > powerUp1.timeToRecharge)
             {
                 powerUp1.abilityRecharging = false;
-                timer = 0;
+                timer1 = 0;
             }
         }
     }
 
     private void PowerUp2Manager()
     {
+        if(coroCop.GetButtonDown("Ability2"))
+        {
+            if (!powerUp2.taserOut)
+            {
+                powerUp2.taserOut = true;
+            }
+            else
+            {
+                powerUp2.taserOut = false;
+            }
+        }
+
+        if(coroCop.GetButtonDown("ShootGun") && powerUp2.taserOut)
+        {
+            powerUp2.hasShot = true;
+
+            
+
+            Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, powerUp2.maxDistance, powerUp2.layermask))
+            {
+                if(hit.collider.tag == "Player")
+                {
+                    //hit.collider.gameObject.GetComponent<FirstPersonController>().Test();
+                }
+            }
+        }
 
     }
     
