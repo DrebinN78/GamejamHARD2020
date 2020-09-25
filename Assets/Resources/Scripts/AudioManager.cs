50a324418ad1e084fb8c7d2e69ad310ac8c12eb8
Dorian <dorian.gelas@gmail.com>
2020-09-25 12:08:29 +0200

    Killsound spatialisation

--- Assets/Resources/Scripts/AudioManager.cs
+++ Assets/Resources/Scripts/AudioManager.cs
@@ -35,6 +35,7 @@ public class AudioManager : MonoBehaviour
             s.source.pitch = s.pitch;
             s.source.loop = s.loop;
             s.source.spatialize = false;
+            s.source.spatialBlend = 1f;
         }
         //MusicPrefab = Resources.Load<GameObject>("Prefab/Musique");
     }
